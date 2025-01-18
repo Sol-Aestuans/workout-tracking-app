de6bc710889308c68e5dcea6c484bd55e9f65d7d
Sol-Aestuans <Aidansol@outlook.com>
2025-01-17 20:30:48 -0800

    listBuilder used to generate exercise list

--- app/lib/pages/workout_builder_page.dart
+++ app/lib/pages/workout_builder_page.dart
@@ -10,6 +10,14 @@ class WorkoutBuilderPage extends StatefulWidget {
 }
 
 class _WorkoutBuilderPageState extends State<WorkoutBuilderPage> {
+  
+  List<String> exercises = [];
+
+  void addExercise() {
+    setState(() {
+      exercises.add("Exercise ${exercises.length + 1}");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +27,30 @@ class _WorkoutBuilderPageState extends State<WorkoutBuilderPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
         actions: [
-          IconButton(onPressed: () {print("Account page");}, icon: const Icon(Icons.account_circle)),
+          IconButton(
+            onPressed: () {print("Account page");},
+            icon: const Icon(Icons.account_circle)),
         ],
       ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: addExercise,
+            child: Text("New exercise")
+          ),
+          Expanded(
+            
+            child: ListView.builder(
+              itemCount: exercises.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(exercises[index]),
+                );
+              }
+            ),
+          )
+        ],
+      )
     );
   }
 }
